e1203bbcbae5c5c3c6eb0f288d849b73a396300a
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-02-21 14:45:55 -0600

    Subiendo el panel del administrador

---
+++ admin/assets/statics/footer.php
@@ -0,0 +1,7 @@
+   <!-- footer area start-->
+   <footer>
+       <div class="footer-area">
+           <p>© Copyright 2023. Todos los derechos reservados. Desarrolado por <a href="https://jupitersoft.tech/">JupiterSoft</a>.</p>
+       </div>
+   </footer>
+   <!-- footer area end-->
