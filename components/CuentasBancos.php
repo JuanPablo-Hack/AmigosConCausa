cfa9ef3272314955b04d546fe2b55734b43dd4f9
Kevinmejia12 <kmejia@ucol.mx>
2023-03-23 13:33:00 -0600

    cambios visuales en cuentas y boletos

--- components/CuentasBancos.php
+++ components/CuentasBancos.php
@@ -20,61 +20,48 @@
                             compañía de la sociedad.
                         </p>
                     </div>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="service-box wow zoomIn" data-wow-duration="1.5s" data-wow-delay="0.1s">
-                        <img src="./assets/img/bbva.png" alt="" />
-                        <h3>
-                            Bancomer
-                        </h3>
-                        <div class="text-left">
-                            <h3><strong>No tarjeta</strong></h3>
-                            <p>
-                                4152 3140 4736 5039
-                            </p>
-                            <h3><strong>Clabe</strong></h3>
-                            <p>
-                                012180015467932106
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="service-box wow zoomIn" data-wow-duration="1.5s" data-wow-delay="0.1s">
-                        <img src="./assets/img/banamex.png" alt="" />
-                        <h3>
-                            Banamex
-
-                        </h3>
-                        <div class="text-left">
-                            <h3><strong>No tarjeta</strong></h3>
-                            <p>
-                                5256 7822 4048 8898
-                            </p>
-                            <h3><strong>Clabe Interbancaria</strong></h3>
-                            <p>
-                                002095901605266526
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="service-box wow zoomIn" data-wow-duration="1.5s" data-wow-delay="0.1s">
-                        <img src="./assets/img/banorte.png" alt="" />
-                        <h3>
-                            Banorte
-                        </h3>
-                        <div class="text-left">
-                            <h3><strong>No tarjeta</strong></h3>
-                            <p>
-                                4915 6630 5495 0459
-                            </p>
-                            <h3><strong>Clabe</strong></h3>
-                            <p>
-                                072095010846994507
-                            </p>
-                        </div>
-                    </div>
+                    <table class="tablaC">
+                        <tr>
+                            <th>BANCO</th>
+                            <th>CUENTA</th>
+                            <th>C.L.A.B.E</th>
+                        </tr>
+                        <tr>
+                            <td><img src="./assets/img/banamex.png"></td>
+                            <td>3-805496680</td>
+                            <td>0-02320038054966808</td>
+                        </tr>
+                        <tr>
+                            <td><img src="./assets/img/bbva.png"></td>
+                            <td>0-452847353</td>
+                            <td>0-12320004528473537</td>
+                        </tr>
+                        <tr>
+                            <td><img src="./assets/img/santander.png"></td>
+                            <td>6-5500776119</td>
+                            <td>0-14320655007761191</td>
+                        </tr>
+                        <tr>
+                            <td><img src="./assets/img/scotiabank.png"></td>
+                            <td>0-1005831202</td>
+                            <td>0-44320010058312023</td>
+                        </tr>
+                        <tr>
+                            <td><img src="./assets/img/HSBC.png"></td>
+                            <td>4-018653535</td>
+                            <td>0-21320040186535354</td>
+                        </tr>
+                        <tr>
+                            <td><img src="./assets/img/banorte.png"></td>
+                            <td>0-313005232</td>
+                            <td>0-72320003130052329</td>
+                        </tr>
+                        <tr>
+                            <td><img src="./assets/img/bansi.png"></td>
+                            <td>0-0096916795</td>
+                            <td>0-60320000969167953</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
