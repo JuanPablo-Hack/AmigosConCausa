4ad4fd86ecfffe057d1794dca81d74b1bb7d14a7
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-03-23 10:58:31 -0600

    Subiendo cambios

--- static/head.php
+++ static/head.php
@@ -3,6 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="description" content="Sorteos con causa para mejorar la vida de la sociedad en base, de juegos interactivos con la gente" />
 <meta name="author" content="Jupiter Soft Tech Company" />
+<meta name="apple-mobile-web-app-capable" content="yes">
 <!-- METAS PARA LA PERZONALIACION DEL LINK -->
 <meta property="og:url" content="https://amigosconcausa.com/" />
 <meta property="og:tittle" content="Amigos Con Causa" />
@@ -10,9 +11,9 @@
 <meta property="og:image" content="https://amigosconcausa.com/assets/img/logo_og.png" />
 
 <link rel="manifest" href="manifest.json" />
+<!-- PWA ICONS SECTION -->
 <link rel="shortcut icon" href="assets/img/logo.ico">
 <link href="assets/img/logo.ico" rel="icon">
-<!-- PWA ICONS SECTION -->
 <link rel="apple-touch-icon" href="assets/img/icons/logo_72x72.png" />
 <link rel="apple-touch-icon" href="assets/img/icons/logo_96x96.png" />
 <link rel="apple-touch-icon" href="assets/img/icons/logo_128x128.png" />
@@ -29,7 +30,6 @@
 <link rel="apple-touch-startup-image" href="assets/img/splash/launch-1536x2048.png" media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
 <link rel="apple-touch-startup-image" href="assets/img/splash/launch-1668x2224.png" media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
 <link rel="apple-touch-startup-image" href="assets/img/splash/launch-2048x2732.png" media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
-<meta name="apple-mobile-web-app-capable" content="yes">
 <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
 <link rel="stylesheet" href="assets/css/animate.min.css" />
 <link rel="stylesheet" href="assets/css/font-awesome.min.css" />
