f35b556bec384cd3604aa7fc96f2976179d3571c
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-03-23 10:27:50 -0600

    Launch laps

--- static/head.php
+++ static/head.php
@@ -2,10 +2,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="description" content="Sorteos con causa para mejorar la vida de la sociedad en base, de juegos interactivos con la gente" />
 <meta name="author" content="Jupiter Soft Tech Company" />
+<meta name="apple-mobile-web-app-capable" content="yes">
 <link rel="manifest" href="manifest.json" />
 <title>Amigos con causa</title>
 <link rel="shortcut icon" href="assets/img/logo.ico">
 <link href="assets/img/logo.ico" rel="icon">
+<!-- PWA ICONS SECTION -->
 <link rel="apple-touch-icon" href="assets/img/icons/logo_72x72.png" />
 <link rel="apple-touch-icon" href="assets/img/icons/logo_96x96.png" />
 <link rel="apple-touch-icon" href="assets/img/icons/logo_128x128.png" />
@@ -14,6 +16,15 @@
 <link rel="apple-touch-icon" href="assets/img/icons/logo_192x192.png" />
 <link rel="apple-touch-icon" href="assets/img/icons/logo_384x384.png" />
 <link rel="apple-touch-icon" href="assets/img/icons/logo_512x512.png" />
+<!-- PWA LAUNCH SECTION ICON -->
+<link rel="apple-touch-startup-image" href="assets/img/splash/launch-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+<link rel="apple-touch-startup-image" href="assets/img/splash/launch-750x1294.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+<link rel="apple-touch-startup-image" href="assets/img/splash/launch-1242x2148.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+<link rel="apple-touch-startup-image" href="assets/img/splash/launch-1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+<link rel="apple-touch-startup-image" href="assets/img/splash/launch-1536x2048.png" media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+<link rel="apple-touch-startup-image" href="assets/img/splash/launch-1668x2224.png" media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+<link rel="apple-touch-startup-image" href="assets/img/splash/launch-2048x2732.png" media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+
 <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
 <link rel="stylesheet" href="assets/css/animate.min.css" />
 <link rel="stylesheet" href="assets/css/font-awesome.min.css" />
