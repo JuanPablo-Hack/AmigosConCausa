e26ac0428eaf96000f2b0711715624d77e190f68
JuanPabloHack <juanpablodejesusfigueroa@gmail.com>
2023-04-22 17:43:27 -0600

    Tabla cuentas#2

--- components/CuentasBancos.php
+++ components/CuentasBancos.php
@@ -30,22 +30,22 @@
                        <tbody>
                             <tr>
                                 <td><img class="img_cuentas" src="./assets/img/banamex.png" alt="banamex"></td>
-                                <td>5256782240488898</td>
+                                <td>5256 7822 4084 8898</td>
                                 <td>002095901605266526</td>
                             </tr>
                             <tr>
                                 <td><img class="img_cuentas" src="./assets/img/bbva.png" alt="bbva"></td>
-                                <td>4152314047365039</td>
+                                <td>4152 3140 4736 5093</td>
                                 <td>012180015467932106</td>
                             </tr>
                             <tr>
                                 <td><img class="img_cuentas" src="./assets/img/banorte.png" alt="banorte"></td>
-                                <td>4915663054930459</td>
+                                <td>4915 6630 5493 0459</td>
                                 <td>0720950108469994507</td>
                             </tr>
                             <tr>
                                 <td><img class="img_cuentas" src="./assets/img/banorte.png" alt="banorte"></td>
-                                <td>4189143051430570</td>
+                                <td>4189 1430 5143 0570</td>
                                 <td>0720950112059805973</td>
                             </tr>
                        </tbody>
