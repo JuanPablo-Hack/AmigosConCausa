5f86980be81d26cb3813731a8a6a9eee82aff867
Kevinmejia12 <kevinjaime0104@gmail.com>
2023-04-28 14:50:17 -0600

    msm para confirmar y cancelar

--- controllers/orden_controller.php
+++ controllers/orden_controller.php
@@ -6,7 +6,7 @@ include "../config/conexion.php";
 $nombre = $_POST["name"];
 $email = $_POST["email"];
 $tel = $_POST["phone"];
-$numeros_seleccionados = $_POST["numerc2b2d2f78d4108302ae2a95734fd4858os"];
+$numeros_seleccionados = $_POST["numeros"];
 
 $SQL = "INSERT INTO `info_registros` (`id`, `numeros_seleccionado`, `nombre`, `tel`, `email`, `id_estado`, `fecha_registro`) VALUES (NULL, '$numeros_seleccionados', '$nombre ', '$tel', '$email', '1', current_timestamp())";
 $resultado = $conexion->query($SQL);
--- sendSMS.php
+++ sendSMS.php
@@ -1,7 +1,7 @@
 <?php
 use Twilio\Rest\Client;
 
-function sendSMS($phone,$numeros){
+function sendSMS($tel,$numeros){
     // Include the bundled autoload from the Twilio PHP Helper Library
     require __DIR__ . '/twilio-php/src/Twilio/autoload.php';
     // Your Account SID and Auth Token from twilio.com/console
@@ -14,11 +14,12 @@ function sendSMS($phone,$numeros){
     $client = new Client($account_sid, $auth_token);
     $client->messages->create(
         // Where to send a text message (your cell phone?)
-        '+52'.$phone,
+        '+52'.$tel,
         array(
             'from' => $twilio_number,
             'body' => 'Pedido confirmado, tus boletos son: '.$numeros. "|| ¡Procede con el pago para confirmar tus numeros!"
         )
     );
-}
+};
+
 ?>
