f1a50f8b9c42928d4e12d602ff9d5b51695f267d
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-04-28 13:56:53 -0600

    Subiendo cambios del señor

--- controllers/orden_controller.php
+++ controllers/orden_controller.php
@@ -2,15 +2,11 @@
 
 include "../sendSMS.php";
 include "../config/conexion.php";
-
 $nombre = $_POST["name"];
 $email = $_POST["email"];
 $tel = $_POST["phone"];
-$numeros_seleccionados = $_POST["numerc2b2d2f78d4108302ae2a95734fd4858os"];
-
+$numeros_seleccionados = $_POST["numeros"];
 $SQL = "INSERT INTO `info_registros` (`id`, `numeros_seleccionado`, `nombre`, `tel`, `email`, `id_estado`, `fecha_registro`) VALUES (NULL, '$numeros_seleccionados', '$nombre ', '$tel', '$email', '1', current_timestamp())";
 $resultado = $conexion->query($SQL);
 
-sendSMS($tel,$numeros_seleccionados);
-
-?>
+sendSMS($tel, $numeros_seleccionados);
--- sendSMS.php
+++ sendSMS.php
@@ -1,24 +1,19 @@
 <?php
+
 use Twilio\Rest\Client;
 
-function sendSMS($phone,$numeros){
-    // Include the bundled autoload from the Twilio PHP Helper Library
+function sendSMS($phone, $numeros)
+{
     require __DIR__ . '/twilio-php/src/Twilio/autoload.php';
-    // Your Account SID and Auth Token from twilio.com/console
     $account_sid = 'AC33ca75b9f9374df2584752cf94252d76';
     $auth_token = 'c2b2d2f78d4108302ae2a95734fd4858';
-    // In production, these should be environment variables. E.g.:
-    // $auth_token = $_ENV["TWILIO_ACCOUNT_SID"]
-    // A Twilio number you own with SMS capabilities
     $twilio_number = "+16203372385";
     $client = new Client($account_sid, $auth_token);
     $client->messages->create(
-        // Where to send a text message (your cell phone?)
-        '+52'.$phone,
+        '+52' . $phone,
         array(
             'from' => $twilio_number,
-            'body' => 'Pedido confirmado, tus boletos son: '.$numeros. "|| ¡Procede con el pago para confirmar tus numeros!"
+            'body' => 'Pedido confirmado, tus boletos son: ' . $numeros . "|| ¡Procede con el pago para confirmar tus numeros!"
         )
     );
 }
-?>
