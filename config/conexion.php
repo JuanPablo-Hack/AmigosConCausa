91cabbff229acf6a388e7f8b6e525a18b4cf129a
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-05-12 13:32:12 -0600

    Subiendo todas las correcciones

--- config/conexion.php
+++ config/conexion.php
@@ -1,3 +1,3 @@
 <?php
-
-$conexion = mysqli_connect("sql726.main-hosting.eu", "u288448544_control", "YiXT>qZ6+v", "u288448544_control");
+include 'userDBInfo.php';
+$conexion = mysqli_connect(SERVER, USER, PASSWORD, DBNAME);
---
+++ config/userDBInfo.php
@@ -0,0 +1,5 @@
+<?php
+define("SERVER", 'sql726.main-hosting.eu');
+define("USER", 'u288448544_control');
+define("PASSWORD", "YiXT>qZ6+v");
+define("DBNAME", "u288448544_control");
